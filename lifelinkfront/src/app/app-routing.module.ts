a8036055e95772ef56d8b0904ffdc3c36ac9ad8d
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-18 14:26:04 +0530

    acceptchanges

--- lifelinkfront/src/app/app-routing.module.ts
+++ lifelinkfront/src/app/app-routing.module.ts
@@ -4,14 +4,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { SingupComponent } from './pages/singup/singup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { InstitutionComponent } from './pages/institution/institution.component';
-<<<<<<< HEAD
-import { OrganComponent } from './pages/organ/organ.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
-=======
-import { HomepageComponent } from './pages/homepage/homepage.component';
->>>>>>> b97a86b346905ea9734226750628db6ca794bfbb
 
 
 const routes: Routes = [
