bc84c275d7fd35cbf3e5771051a447a8d801d9c6
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-18 11:19:15 +0530

    dash

--- lifelinkfront/src/app/pages/dashboard/dashboard.component.ts
+++ lifelinkfront/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+// dashboard.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import baseUrl from '../../services/helper';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  organizations: any[] = [];
 
+  constructor(private http: HttpClient,private router:Router) { }
+
+  ngOnInit(): void {
+    this.getOrganizations();
+  }
+
+  getOrganizations(): void {
+    this.http.get<any[]>(`${baseUrl}/get-all-inst`).subscribe(
+      (response) => {
+        this.organizations = response;
+        console.log(response)
+      },
+      (error) => {
+        console.error('Error fetching organizations:', error);
+      }
+    );
+  }
+
+  public navigateToOrganization(instName:any){
+
+  this.router.navigateByUrl("/inst-page")    
+
+  }
 }
